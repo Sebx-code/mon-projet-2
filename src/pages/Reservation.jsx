7b38f141e1d6a7dee628bb7368ab995ac6f8ae48
Sebx-code <steevebille1@gmail.com>
2025-10-28 15:59:46 +0100

    modified 3

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -5,23 +5,9 @@ import db from "../../db.json";
 import Dot from "../assets/DashboardIconDot";
 
 function Reserv({ onNavigateToForm }) {
-  const getStatusColor = (status) => {
-    switch (status) {
-      case "Disponible":
-        return "bg-green-600/20 text-green-400";
-      case "Louée":
-        return "bg-orange-600/20 text-orange-400";
-      case "En maintenance":
-        return "bg-red-600/20 text-red-400";
-      default:
-        return "bg-gray-700 text-gray-300";
-    }
-  };
-
   return (
     <>
-      <div className="p-6 bg-gray-900 text-gray-200 min-h-screen flex flex-col justify-between items-center">
-        <div className="">hgfhjgfhgfhg</div>
+      <div className="p-6 bg-gray-900 text-gray-200 min-h-screen flex flex-col justify-between items-center text-sm w-1xl">
         <div className="overflow-y-auto rounded-3xl overflow-hidden">
           <div className="px-6 py-4 border-b border-gray-200 dark:border-gray-700">
             <div className="flex items-center justify-between">
@@ -34,35 +20,55 @@ function Reserv({ onNavigateToForm }) {
                 </button>
                 <button
                   className="px-3 py-1.5 text-xs font-medium text-white bg-primary-600 hover:bg-primary-700 rounded-lg transition-colors"
-                  onClick={onNavigateToForm}
-                >
+                  onClick={onNavigateToForm}>
                   + Ajouter
                 </button>
               </div>
             </div>
           </div>
 
-          <table className="w-full text-left border-collapse rounded-3xl overflow-hidden border border-gray-500">
+          <div className="p-15 bg-gray-900 text-gray-200 min-h-screen flex flex-col justify-between items-center overflow-x-auto overflow-y-auto">
+            <table className=" text-left border-collapse rounded-3xl overflow-hidden border border-gray-500">
             <thead>
               <tr className="text-gray-400 text-sm bg-gray-900">
-                <th className="px-4 py-2 border border-gray-500">Car number</th>
-                <th className="px-4 py-2 border border-gray-500">Car's type</th>
-                <th className="px-4 py-2 border border-gray-500">Car model</th>
+                <th className="px-4 py-2 border border-gray-500">Id</th>
                 <th className="px-4 py-2 border border-gray-500">
-                  License plate
+                  Nom du client
                 </th>
-                <th className="px-4 py-2 border border-gray-500">Year</th>
-                <th className="px-4 py-2 border border-gray-500">Color</th>
-                <th className="px-4 py-2 border border-gray-500">Mileage</th>
                 <th className="px-4 py-2 border border-gray-500">
-                  Daily price
+                  Numéro de pièce d'identité
                 </th>
-                <th className="px-4 py-2 border border-gray-500">Status</th>
+                <th className="px-4 py-2 border border-gray-500">
+                  Téléphone du client
+                </th>
+                <th className="px-4 py-2 border border-gray-500">
+                  Adresse du client
+                </th>
+                <th className="px-4 py-2 border border-gray-500">
+                  Modèle de la voiture
+                </th>
+                <th className="px-4 py-2 border border-gray-500">
+                  Immatriculation du véhicule
+                </th>
+                <th className="px-4 py-2 border border-gray-500">
+                  Date de début
+                </th>
+                <th className="px-4 py-2 border border-gray-500">
+                  Date de fin
+                </th>
+                <th className="px-4 py-2 border border-gray-500">
+                  Prix par jour
+                </th>
+                <th className="px-4 py-2 border border-gray-500">Prix total</th>
+                <th className="px-4 py-2 border border-gray-500">Caution</th>
+                <th className="px-4 py-2 border border-gray-500">
+                  Mode de paiement
+                </th>
                 <th className="px-4 py-2 border border-gray-500">Action</th>
               </tr>
             </thead>
             <tbody>
-              {db.Ajout_Voiture.slice()
+              {db.Location_Voiture.slice()
                 .reverse()
                 .map((row) => (
                   <tr
@@ -73,34 +79,40 @@ function Reserv({ onNavigateToForm }) {
                       {row.id}
                     </td>
                     <td className="px-4 py-3 border border-gray-500">
-                      <p className="font-medium">{row.car_type}</p>
+                      <p className="font-medium">{row.client_name}</p>
                     </td>
                     <td className="px-4 py-3 border border-gray-500">
-                      <p className="font-medium">{row.car_model}</p>
+                      <p className="font-medium">{row.identity_number}</p>
                     </td>
                     <td className="px-4 py-3 border border-gray-500">
-                      {row.license_plate}
+                      {row.client_phone}
                     </td>
                     <td className="px-4 py-3 border border-gray-500">
-                      {row.year}
+                      {row.client_address}
                     </td>
                     <td className="px-4 py-3 truncate max-w-xs border border-gray-500">
-                      {row.color}
+                      {row.car_model}
                     </td>
                     <td className="px-4 py-3 font-semibold border border-gray-500">
-                      {row.mileage}
+                      {row.license_plate}
+                    </td>
+                    <td className="px-4 py-3 font-semibold border border-gray-500">
+                      {row.rental_start}
+                    </td>
+                    <td className="px-4 py-3 font-semibold border border-gray-500">
+                      {row.rental_end}
                     </td>
                     <td className="px-4 py-3 font-semibold border border-gray-500">
                       {row.daily_price}
                     </td>
-                    <td className="px-4 py-3 border border-gray-500">
-                      <span
-                        className={`px-3 py-1 rounded-full text-[7px] font-semibold ${getStatusColor(
-                          row.status
-                        )}`}
-                      >
-                        {row.status}
-                      </span>
+                    <td className="px-4 py-3 font-semibold border border-gray-500">
+                      {row.total_price}
+                    </td>
+                    <td className="px-4 py-3 font-semibold border border-gray-500">
+                      {row.caution}
+                    </td>
+                    <td className="px-4 py-3 font-semibold border border-gray-500">
+                      {row.payment_method}
                     </td>
                     <td className="px-4 py-3 text-right border border-gray-500">
                       <button className="text-gray-400 hover:text-gray-600 dark:hover:text-gray-300 transition-colors p-1 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
@@ -111,6 +123,7 @@ function Reserv({ onNavigateToForm }) {
                 ))}
             </tbody>
           </table>
+          </div>
         </div>
       </div>
     </>
